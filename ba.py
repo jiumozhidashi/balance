d74714317345383015deba899f5fa95aaedaf4f8
Jacky.lei <20826250@qq.com>
2018-06-22 00:24:16 +0800

    v0.002

--- ba.py
+++ ba.py
@@ -1,8 +1,7 @@
 import pandas as pd
+import numpy as np
 
-df=pd.read_excel('zhuanxian.xlsx')
-print(df.head())
-print(df.columns)
-print(df['数量'].max())
-print(df['数量'].min())
-print(df['数量'].mean())
+df1=pd.read_excel('zhuanxian.xlsx')
+'''df2=pd.read_excel('tasklist.xlsx')
+df3=pd.read_excel('uselist.xlsx') '''
+print(df1.groupby('备注').agg({'数量':np.sum}))
